d5940616e597ef3624b7425eea17b5c0d51d0aea
TheEzo <willaschek.t@gmail.com>
2017-04-26 19:06:12 +0200

    a

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
-#include <stdio.h>
+#include <time.h>
+#include <semaphore.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
 
 #define OUTPUT "proj2.out"
 
-int main(int argc, char **argv) {
+void child_gen(int C, int CGT, int CWT);
+void parent_gen(int A, int AGT, int AWT);
+
+sem_t *sem;
+sem_t *sem2;
+int *mem;
+
+int main(int argc, char **argv){
     if(argc != 7){
         fprintf(stderr, "Nespravny pocet argumentu!\n");
         exit(1);
@@ -61,7 +73,55 @@ int main(int argc, char **argv) {
     }
 
 
+    srand(time(NULL));
 
+    setbuf(stdout,NULL);
+    setbuf(stderr,NULL);
+
+    mem = (int*)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    sem = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    sem2 = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    sem_init(sem, 1, 0);
+    sem_init(sem2, 1, 0);
+
+
+
+    pid_t pid;
+
+    if((pid = fork()) < 0){
+        fprintf(stderr, "Fork failed!\n");
+        exit(2);
+    }
+
+    if (pid == 0) { // child generator
+        child_gen(C, CGT, CWT);
+
+        exit(0);
+    }
+    else {
+        if((pid = fork()) < 0){
+            fprintf(stderr, "Fork failed!\n");
+            exit(2);
+        }
+        if (pid == 0) { // adult generator
+            parent_gen(A, AGT, AWT);
+
+
+        }
+    }
+
+    for(int i = 0; i < (A + C); i++) {
+        sem_post(sem2);
+    }
+
+/*for (int i = 0; i < 5; i++) {
+                sem_wait(sem);
+
+                printf("Parent: %d\n", *mem);
+
+                sem_post(sem);
+                usleep(10000);
+            }*/
 
 
     // zavrani souboru
@@ -70,5 +130,128 @@ int main(int argc, char **argv) {
         exit(2);
     }
 
-    return 0;
+    sem_destroy(sem);
+    munmap(mem, sizeof(int));
+    munmap(sem, sizeof(sem_t));
+
+
+
+    exit(0);
 }
+
+/**
+ * Generator deti
+ * @param C Celkovy pocet deti
+ * @param CGT Cas, po kterem se vygeneruje nove dite
+ * @param CWT Cas, po kterem dite opusti kritickou zonu
+ * @param mem Sdilena pamet
+ */
+void child_gen(int C, int CGT, int CWT){
+    for (int i = 0; i < C; i++) {
+        if(CGT > 0) {
+            usleep((rand() % (CGT+1)) * 1000);
+        }
+        printf("%d\t: C %d\t: started\n", (*mem)++, i);
+        sem_wait(sem);
+        printf("%d\t: C %d\t: enter\n", (*mem)++, i);
+        usleep(CWT*100);
+        printf("%d\t: C %d\t: leave\n", (*mem)++, i);
+        sem_post(sem);
+    }
+    return;
+}
+
+/**
+ * Generator rodicu
+ * @param A Celkovy pocet rodicu
+ * @param AGT Cas, po kterem se vygeneruje novy rodic
+ * @param AWT Cas, po kterem se rodic pokousi opustit kritickou zonu
+ * @param mem Sdilena pamet
+ */
+void parent_gen(int A, int AGT, int AWT){
+    if(AGT > 0) {
+        usleep((rand() % (AGT+1)) * 1000);
+    }
+    for(int i = 0; i < A; i++){
+        printf("%d\t: A %d\t: started\n", (*mem)++, i);
+        printf("%d\t: A %d\t: enter\n", (*mem)++, i);
+        for(int j = 0; j < 3; j++) {
+            sem_post(sem);
+        }
+        usleep(AWT);
+        printf("%d\t: A %d\t: trying to leave\n", (*mem)++, i);
+        int *val = (int *)malloc(sizeof(int));
+        sem_getvalue(sem,val);
+        if(*val < 3)
+            printf("%d\t: A %d\t: waiting\n", (*mem)++, i);
+            while(*val >=3)
+                sem_getvalue(sem, val);
+        free(val);
+        for(int j = 0; j < 3; j++)
+            sem_wait(sem);
+
+        printf("%d\t: A %d\t: leave\n", (*mem)++, i);
+
+
+        usleep(50000);
+    }
+    return;
+}
+
+
+/*
+    int pid;
+    setbuf(stdout,NULL);
+    setbuf(stderr,NULL);
+
+    pid_t consPID;
+    pid_t prodPID;
+
+    sem_t *sem;
+    sem = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    sem_init(sem, 1, 0);
+
+    // systemove volani - vzdy je vhodne overit uspesnost!
+    if ((pid = fork()) < 0) {
+        perror("fork");
+        exit(2);
+    }
+
+    if (pid == 0) { // child
+        // cekame, az nam druhy proces dovoli pokracovat
+        sem_wait(sem);
+        for (int i=0; i<10; i++) {
+            fprintf(stdout, "first: %d\n", i);
+            usleep(500);
+        }
+        sem_post(sem);
+        fprintf(stdout, "first: end\n");
+        exit(0);
+    } else { // parent.
+        consPID = pid;
+        //--
+        pid = fork();
+        //--
+        if (pid == 0) { // child
+            for (int i=0; i<10; i++) {
+                fprintf(stdout, "second: %d\n", i);
+                // po splneni podminky dovolime dalsimu procesu pokracovat
+                if (i==5) sem_post(sem);
+                usleep(500);
+            }
+            sem_wait(sem);
+            fprintf(stdout, "second: end\n");
+            exit(0);
+        } else { // parent
+            prodPID = pid;
+        }
+    }
+
+    // pockame az vsichni skonci
+    waitpid(consPID, NULL, 0);
+    waitpid(prodPID, NULL, 0);
+
+    // zrusime zdroje
+    sem_destroy(sem);
+    munmap(sem, sizeof(sem_t));
+    */
